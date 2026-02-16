5c523808de273f8eb1af770a519d69e70a4d9ba8
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-16 11:50:34 +0000

    Added Components Folder

---
+++ components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-small md:text-base cursor-poiter">
+      Free Delivery On Monday
+    </div>
+  );
+};
+
+export default Notification;
