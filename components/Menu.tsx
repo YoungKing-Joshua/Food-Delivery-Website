010fe4e913166d0804f6509194911544f3cbaf83
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-16 16:29:00 +0000

    Updated UI

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import Carticon from "./Carticon";
 
 const links = [
   { id: 1, name: "Homepage", url: "/" },
@@ -14,6 +15,9 @@ const links = [
 const Menu = () => {
   const [open, setOpen] = useState(false);
 
+  //TEMPORARY
+
+  const user = false;
   return (
     <div>
       {!open ? (
@@ -33,11 +37,27 @@ const Menu = () => {
           onClick={() => setOpen(false)}
         />
       )}
-      <div className="bg-red-500 text-white absolute left--0 top-24 h-[calc(100vh-6rem)]">
-        {links.map((item) => (
-          <Link href={item.url}>{item.title}</Link>
-        ))}
-      </div>
+      {open && (
+        <div className="bg-red-500 text-white absolute left--0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
+          ))}
+          {!user ? (
+            <Link href="/login" onClick={() => setOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <Link href="/orders" onClick={() => setOpen(false)}>
+              Orders
+            </Link>
+          )}
+          <Link href="/cart" onClick={() => setOpen(false)}>
+            <Carticon />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,31 @@ import Menu from "./Menu";
 import Link from "next/link";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4">
+        <Link href="/">Homepage</Link>
+        <Link href="/">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/*Logo*/}
       <div className="text-xl">
-        <Link href="">Massino</Link>
+        <Link href="/">Massino</Link>
       </div>
       {/*Mobile Menu*/}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4">
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <Link href="/contact">Contact</Link>
       </div>
     </div>
   );
