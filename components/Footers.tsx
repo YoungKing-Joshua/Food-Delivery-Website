3b3a91ca83cd8e3f65d8eaf4dceb476ed556b903
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-17 11:16:31 +0000

    Updated UI

--- components/Footers.tsx
+++ components/Footers.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Footers = () => {
-  return <div></div>;
+  return (
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center ">
+      <Link href="/">MASSIMO</Link>
+      <p>© ALL RIGHTS RESERVED</p>
+    </div>
+  );
 };
 
 export default Footers;
