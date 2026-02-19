20fbaeaa4b131f9e90684bc181b239d0e7b6d5f1
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-19 14:39:16 +0000

    Updated Cart page

---
+++ app/LoginPage/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+      {/* BOX  */}
+      <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full">
+        {/* IMAGE CONTAINER  */}
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+          <Image src="/loginBg.png" alt="" fill className="object-cover" />
+        </div>
+        {/* FORM CONTAINER  */}
+        <div className="p-10 flex flex-col gap-8">
+          <h1 className="font-bold text-xl">Welcome</h1>
+          <p className="">
+            Log into your account or create a new one using social buttons
+          </p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image
+              src="/google.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            Sign in with Google
+          </button>
+          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+            <Image
+              src="/facebook.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            Sign in with Facebook
+          </button>
+          <p className="text-sm">
+            Have a problem?
+            <Link className="underline" href="/">
+              {" "}
+              Contact us
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
