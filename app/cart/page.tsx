56e7a0e3651c2dc2e2bd4ca6e6eb87cdea7b4936
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-19 13:33:05 +0000

    Updated Cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500">
+      {/* PRODUCTS CONTAINER */}
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll">
+        {/* SINGLE ITEM  */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold ">$79.90</h2>
+          <span className="cursor-pointer">X</span>
+        </div>{" "}
+        {/* SINGLE ITEM  */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold ">$79.90</h2>
+          <span className="cursor-pointer">X</span>
+        </div>{" "}
+        {/* SINGLE ITEM  */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold ">$79.90</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+      </div>
+      {/* PAYMENT CONTAINER*/}
+      <div className="h-1/2 p-4 bg-fuchsia-50">
+        <div className="">
+          <span className="">Subtotal</span>
+          <span className="">$81.70</span>
+        </div>
+        <hr className="" />
+        <button className="bg-red-500 text-white p-3 rounded-md w-1/2">
+          CHECKOUT
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
