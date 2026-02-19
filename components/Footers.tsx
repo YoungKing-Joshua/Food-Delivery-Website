5d3024c7b88aa7fec8e7514c3b2a34ced7dd2840
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-19 17:43:57 +0000

    Updated Footer page

--- components/Footers.tsx
+++ components/Footers.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Footers = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center ">
-      <Link href="/">MASSIMO</Link>
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:p-40 text-red-500 flex items-center justify-between ">
+      <Link href="/" className="font-bond text-xl">
+        MASSIMO
+      </Link>
       <p>© ALL RIGHTS RESERVED</p>
     </div>
   );
