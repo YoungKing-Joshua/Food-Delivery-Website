ec28d86b1756714af3dcbff0f6de63fa200d45f7
Joshua Dei-Alorse <joshuadeialorse@gmail.com>
2026-02-25 10:59:18 +0000

    fixing few issues

--- components/Carticon.tsx
+++ components/Carticon.tsx
@@ -2,15 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Carticon = () => {
-  return (
-    <Link href="/cart" className="flex items-center gap-4">
+type CarticonProps = {
+  withLink?: boolean;
+};
+
+const Carticon = ({ withLink = true }: CarticonProps) => {
+  const content = (
+    <div className="flex items-center gap-4">
       <div className="relative w-8 h-8 md:w-5 md:h-5">
-        <Image src="/cart.png" alt="" fill />
+        <Image src="/cart.png" alt="Cart" fill />
       </div>
       <span>Cart (3)</span>
-    </Link>
+    </div>
   );
+
+  if (withLink) {
+    return <Link href="/cart">{content}</Link>;
+  }
+
+  return content;
 };
 
 export default Carticon;
+
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+
+// const Carticon = () => {
+//   return (
+//     <Link href="/cart" className="flex items-center gap-4">
+//       <div className="relative w-8 h-8 md:w-5 md:h-5">
+//         <Image src="/cart.png" alt="" fill />
+//       </div>
+//       <span>Cart (3)</span>
+//     </Link>
+//   );
+// };
+
+// export default Carticon;
